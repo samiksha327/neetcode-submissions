61ac311bd214dae0b2f68c91e4c08def5a8749dd
samiksha327 <76674914+samiksha327@users.noreply.github.com>
2026-07-03 12:06:13 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+    
+    HashSet<Integer> set1= new HashSet<>();
+    for(int i=0;i<nums.length;i++){
+
+        if(set1.contains(nums[i])){
+            return true;
+
+        }
+          set1.add(nums[i]);
+
+    }
+
+          return false; 
+
+
+    }
+}
